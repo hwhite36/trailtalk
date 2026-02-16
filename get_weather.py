0f3729fd0f035393744f313744357b59407b8095
Harrison White <harrisonw36@gmail.com>
2026-02-16 18:59:12 -0500

    flesh out noaa api communication

--- get_weather.py
+++ get_weather.py
@@ -4,17 +4,17 @@ import requests
 # Define the get_weather tool so Gemini knows how to use it
 get_weather_declaration = {
     "name": "get_weather",
-    "description": "Fetch the weather for a specific location defined via coordinates",
+    "description": "Fetch the hourly and week's weather forecast for a specific location defined via coordinates",
     "parameters": {
         "type": "object",
         "properties": {
             "latitude": {
                 "type": "number",
-                "description": "Latitude coordinate of the location for which to fetch the weather",
+                "description": "Latitude of the location for which to fetch the weather",
             },
             "longitude": {
                 "type": "number",
-                "description": "Longitude coordinate of the location for which to fetch the weather",
+                "description": "Longitude of the location for which to fetch the weather",
             },
         },
         "required": ["latitude", "longitude"],
@@ -24,4 +24,48 @@ get_weather_declaration = {
 weather_tool = types.Tool(function_declarations=[types.FunctionDeclaration(**get_weather_declaration)])
 
 def get_weather(latitude: float, longitude: float):
-    pass
+    """
+    Fetch the hourly and week's weather forecast from NOAA
+    There are 3 API calls to the NOAA API:
+    1. Fetch the forecast zone URLs from the coordinates endpoint, which gives us the other two endpoints to hit
+    2. Fetch the general (week's) forecast
+    3. Fetch the hourly forecast
+
+    :param latitude: latitude coordinate of the location for which to fetch the weather
+    :param longitude: longitude coordinate of the location for which to fetch the weather
+    :return: a dictionary with the hourly and week's weather forecast
+    """
+    # NOAA API doesn't require any auth or API keys lol, but we'll include a User-Agent as a gesture of good faith
+    headers = {"Accept": "application/geo+json", "User-Agent": "WildernessAssistantWeatherBot/1.0"}
+    coords_metadata_url = f"https://api.weather.gov/points/{latitude},{longitude}"
+
+    try:
+        # First we have to get the 2.5km "zone" forecast link from the user's coords
+        coords_metadata_resp = requests.get(coords_metadata_url, headers=headers)
+        coords_metadata_resp.raise_for_status()
+        coords_metadata_json = coords_metadata_resp.json()
+        general_forecast_url = coords_metadata_json["properties"]["forecast"]
+        hourly_forecast_url = coords_metadata_json["properties"]["forecastHourly"]
+        if not general_forecast_url or not hourly_forecast_url:
+            return {"Error": "Error fetching forecast URL"}
+
+        # Now we fetch the actual forecast data
+        gen_forecast_resp = requests.get(general_forecast_url, headers=headers)
+        gen_forecast_resp.raise_for_status()
+        gen_forecast_json = gen_forecast_resp.json()
+        hourly_forecast_resp = requests.get(hourly_forecast_url, headers=headers)
+        hourly_forecast_resp.raise_for_status()
+        hourly_forecast_json = hourly_forecast_resp.json()
+
+        # only give 24 hrs of hourly data to the model
+        hourly_forecast_24hrs: list = hourly_forecast_json["properties"]["periods"][:24]
+        week_forecast: list = gen_forecast_json["properties"]["periods"]
+
+        return {"Hourly forecast": hourly_forecast_24hrs, "Week's forecast": week_forecast}
+
+    except requests.exceptions.HTTPError as e:
+        # TODO update to logger
+        print(e)
+        if e.response.status_code == 404:
+            return {"Error": "No NOAA weather forecast available for that location"}
+        return {"Error": f"Could not fetch weather data: {e}"}
